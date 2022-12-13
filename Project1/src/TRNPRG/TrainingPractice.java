6e4b1d552e7dbff386ee82a0d8603c602460b95e
PUSHPENDRA <pushpendrakumar91197@gmail.com>
2022-12-13 12:37:04 +0530

    This is from master

---
+++ Project1/src/TRNPRG/TrainingPractice.java
@@ -0,0 +1,11 @@
+package TRNPRG;
+
+public class TrainingPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is Master Branch content");
+
+	}
+
+}
